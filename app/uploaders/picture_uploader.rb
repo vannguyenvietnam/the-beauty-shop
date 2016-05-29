d40cf452ce9a3d37b88428246259d6f68bf95804
vannguyenvietnam <bichvannguyenvnn@gmail.com>
2016-05-29 15:55:57 +0700

    Changed uploader storage on file'

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -9,11 +9,12 @@ class PictureUploader < CarrierWave::Uploader::Base
 
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  #if Rails.env.production?
+  #  storage :fog
+  #else
+  #  storage :file
+  #end
+  storage :file
 
 
   # Override the directory where uploaded files will be stored.
